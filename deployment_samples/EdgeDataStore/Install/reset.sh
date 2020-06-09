a34b969a5edc3104d441697e4f48788bddac8260
Doug Broad <dbroad@osisoft.com>
2020-06-09 10:48:18 -0400

    Try pipeline fixes

--- deployment_samples/EdgeDataStore/Install/reset.sh
+++ deployment_samples/EdgeDataStore/Install/reset.sh
@@ -2,7 +2,6 @@ echo "Reset: Read settings from config.ini..."
 source <(grep = config.ini | tr -d "\r")
 
 echo "Reset: Send device reset script to device..."
-echo scp -r reset-device.sh $UserName@$IpAddress:/usr/local/install
 scp -r reset-device.sh $UserName@$IpAddress:/usr/local/install
 
 echo "Reset: Running device reset script..."
